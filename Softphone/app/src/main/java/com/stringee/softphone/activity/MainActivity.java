992ab3573dc83e06235058cddff32e73ba139ecc
Luan Nguyen <luan@stringee.com>
2019-10-28 11:22:48 +0700

    Update SDK to 1.3.10

--- Softphone/app/src/main/java/com/stringee/softphone/activity/MainActivity.java
+++ Softphone/app/src/main/java/com/stringee/softphone/activity/MainActivity.java
@@ -354,6 +354,11 @@ public class MainActivity extends MActivity {
             public void onCustomMessage(String s, JSONObject jsonObject) {
 
             }
+
+            @Override
+            public void onTopicMessage(String s, JSONObject jsonObject) {
+
+            }
         });
     }
 
